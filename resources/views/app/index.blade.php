6a7dc6b17dffee51b89a39c9466fcd7c3e76f7cd
yuhaoming <510559413@qq.com>
2017-11-20 16:09:01 +0800

    1。上线准备

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -5,17 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel React</title>
   <link href='http://cdn.webfont.youziku.com/webfonts/nomal/110736/45817/5a105e58f629d804a00a92f1.css' rel='stylesheet' type='text/css' />
-  <link rel="shortcut icon" href="{{assets('/app/favicon.ico')}}">
-  <link rel="stylesheet" href="{{assets('/app/index.css')}}" />
-  <link rel="stylesheet" href="{{assets('/app/filepicker.css')}}" />
+  <link rel="shortcut icon" href="{{asset('/app/favicon.ico')}}">
+  <link rel="stylesheet" href="{{asset('/app/index.css')}}" />
+  <link rel="stylesheet" href="{{asset('/app/filepicker.css')}}" />
 
 <body>
   <div id="root"></div>
 
-  <script src="{{assets('/app/clipboard.min.js')}}"></script>
+  <script src="{{asset('/app/clipboard.min.js')}}"></script>
 
   <script>
         new Clipboard('.copy-button')
   </script>
-<script type="text/javascript" src="{{assets('/app/common.js')}}"></script><script type="text/javascript" src="/index.js"></script></body>
+<script type="text/javascript" src="{{asset('/app/common.js')}}"></script><script type="text/javascript" src="/index.js"></script></body>
 </html>
