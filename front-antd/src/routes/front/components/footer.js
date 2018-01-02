3149fcca57e0020d28c3923a86b42530ebe0b187
yuhaoming <510559413@qq.com>
2018-01-02 14:10:48 +0800

    1。360网站安全认证

--- front-antd/src/routes/front/components/footer.js
+++ front-antd/src/routes/front/components/footer.js
@@ -54,6 +54,9 @@ const Footer = ({classes})=>{
                                     <Icon style={{color:'#ffffff',fontSize:18,marginRight:12}} type="mail"/>
                                     <a href="" className={classes.flink}>510559413@qq.com</a>
                                 </li>
+                                <li className={classes.footLinksItem}><a href="http://webscan.360.cn/index/checkwebsite/url/www.docwhite.cn">
+                                    <img height={30} src="http://webscan.360.cn/status/pai/hash/742c3054f0d87c2933414d47805d34fd"/></a>
+                                </li>
                             </ul>
                         </div>
                     </Grid>
