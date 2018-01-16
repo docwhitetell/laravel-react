14c16f22aed72aa5857a5780e9644ea5a415227e
yuhaoming <510559413@qq.com>
2018-01-16 11:00:02 +0800

    1.优化数据API，删除冗余数据

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -35,7 +35,7 @@ class BlogController extends Controller
         return response()->json(['success',true],200);
     }
     public function userblogs(Request $request){
-        return $request->user()->blogs()->orderBy('created_at','desc')->get();
+        return $request->user()->blogs()->orderBy('created_at','desc')->select(['id','title','description','poster','classes'])->get();
     }
     public function update(Request $request){
         $data=$request->get('blog');
